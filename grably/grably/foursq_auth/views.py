afbb6d53267d83fe162ccc1b0672ddec2e3de484
Clara Wu <s.clara.wu@gmail.com>
2013-04-06 22:53:59 -0400

    views

--- grably/grably/foursq_auth/views.py
+++ grably/grably/foursq_auth/views.py
@@ -45,7 +45,7 @@ def unauth( request ):
     # clear any tokens and logout
     request.session.clear( )
     logout( request )
-    return HttpResponseRedirect( reverse( 'main' ) )  
+    return HttpResponseRedirect( reverse( 'main' ) )
 
 def auth( request ):
     # build the url to request
@@ -73,3 +73,23 @@ def done( request ):
 
     # show the page with the user's name to show they've logged in
     return render_to_response( 'foursq_auth/done.html', { 'name' : name } )
+
+def get_auth(request):
+    if request.is_ajax():
+        access_token = request.session.get('access_token')
+        return HttpResponse(access_token)
+    else:
+        return HttpResponse(0)
+
+def create_checkin(request):
+    if request.is_ajax():
+        access_token = request.session.get('access_token')
+        venue = request.POST['id']
+        url= 'https://api.foursquare.com/v2/checkins/add'
+        values = {
+            'oauth_token' : access_token,
+            'venueId' : venue,
+        }
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data)
+
