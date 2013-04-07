3871d359a7cf7939f0a01c579e0cfda4bfda3bb8
Clara Wu <s.clara.wu@gmail.com>
2013-04-07 03:22:50 -0400

    make field

--- grably/grably/app/models.py
+++ grably/grably/app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Grabber(models.Model):
    username = models.CharField(max_length = 100) #check for this in the input
    venmo_key = models.CharField(max_length = 100)
-   twitter = models.CharField(max_length = 100)#tweeter handle 
+   twitter = models.CharField(max_length = 100, blank = True, null=True)#tweeter handle
 
 class Tasks(models.Model):
     task_id = models.AutoField(primary_key=True)
