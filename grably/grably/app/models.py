457f5914a8755533b2a4d4c0d64800a0e11ba326
Clara Wu <s.clara.wu@gmail.com>
2013-04-06 23:22:30 -0400

    added auto-increment field to app.models.tasks

--- grably/grably/app/models.py
+++ grably/grably/app/models.py
@@ -6,9 +6,10 @@ class Grabber(models.Model):
    venmo_key = models.CharField(max_length = 100)
 
 class Tasks(models.Model):
-    task_id = models.CharField(max_length = 200)
+    task_id = models.AutoField(primary_key=True)
     task_title = models.CharField(max_length = 100)
     task_description = models.CharField(max_length = 1000)
+    date = models.DateField(auto_now_add=True)
     price = models.IntegerField(default = 0)
     assigner = models.ForeignKey(Grabber, related_name="assigner_id")
     executor = models.ForeignKey(Grabber, related_name="executor_id")
--- grably/grably/app/views.py
+++ grably/grably/app/views.py
@@ -1,1 +1,6 @@
+from app.models import *
 # Create your views here.
+def populate_db(request):
+
+
+
